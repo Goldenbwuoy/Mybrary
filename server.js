c5265cf0af106a4c3d8d91aa2910a8e826336f59
Golden TM <mumanikidzwag@gmail.com>
2020-04-20 20:04:27 +0800

    Added Author routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();;
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true});
@@ -20,7 +23,9 @@ const db = mongoose.connection
 db.on('error', err => console.error(err));
 db.once('open', () => console.log('Connected to mongoose.'));
 
-app.get('/', indexRouter)
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/authors/new', authorRouter)
 
 
 app.listen(process.env.PORT || 3000);
